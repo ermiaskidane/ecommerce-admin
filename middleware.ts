871253d6f187a2edc9d2103d61010bca0ecf0eee
ermias kidane <ermiaskidanegebre@gmail.com>
2023-07-08 19:05:58 +0100

    implement billboard and category pages

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,9 @@
 import { authMiddleware } from "@clerk/nextjs";
 
-export default authMiddleware();
+export default authMiddleware({
+  // this makes the get request accessable to public with out auth
+  publicRoutes: ["/api/:path*"]
+});
 
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
